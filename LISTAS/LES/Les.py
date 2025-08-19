0a1ff699f0f128865b841f2aafaf9560f6372731
NettoZk <adeuvaldon7@gmail.com>
2025-08-19 10:50:58 -0300

    def remover

--- LISTAS/LES/Les.py
+++ LISTAS/LES/Les.py
@@ -6,76 +6,18 @@ class Les:
         self.vetor = [None] * self.tam
         self.quant = 0
         
-        # Inicializa a lista LES com capacidade fixa.
-
-        # Exemplo:
-
-        # tamanho = 5
-
-        # Vetor inicial (tamanho 5, vazio):
-        # Índices:
-        #  _____  _____  _____  _____  _____
-        # |  0  ||  1  ||  2  ||  3  ||  4  |
-        #  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        # Valores:
-        #  _____  _____  _____  _____  _____
-        # |     ||     ||     ||     ||     |
-        #  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-
-        # Quantidade de elementos = 0
-        
     def show(self):
         
         for i in range(self.quant): 
             print(self.vetor[i], end=" ") 
-        print()
-        
-        # Mostra os elementos da lista (do índice 0 até quant-1).
-        
-        # Exemplo visual:
-        # Índices:
-        #   _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        # Valores:
-        #   _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  D  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   
-        
+        print()  
+    
     def inserir_fim(self, valor):
-        
         if self.quant == self.tam:
             print('Não dá krlh, a lista está cheiaaaa')
         else:
             self.vetor[self.quant] = valor
             self.quant += 1
-        
-        # Insere um valor no final da lista, se houver espaço.
-
-        # Exemplo:
-
-        # Antes (quant=3):
-
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ 
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||     ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ 
-
-        # Inserir_fim('D'):
-        
-        # Depois (quant=4):
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  D  ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   
     
     def remover_fim(self):
         
@@ -83,41 +25,7 @@ class Les:
             print('A lista está vazia')
         else:
             self.quant -= 1
-        
-        # Remove o último elemento da lista, se não estiver vazia.
 
-        # Exemplo:
-
-        # Antes (quant=4):
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  D  ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-
-        # Remover_fim():
-
-        # Depois (quant=3):
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  D  ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        # Obs: o valor na posição removida ainda existe no vetor,
-        # mas não é considerado porque quant diminuiu.
-        
-        # Sáida no terminal após show()
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||     ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        
     def inserir_inicio(self, valor):
         
         if self.quant == self.tam:
@@ -127,40 +35,6 @@ class Les:
                 self.vetor[i] = self.vetor[i - 1]
             self.vetor[0] = valor
             self.quant += 1
-        
-        # Insere um elemento no início da lista, movendo todos para a direita.
-
-        # Exemplo:
-
-        # Antes (quant=4):
-
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  E  ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-
-        # Passo a passo do deslocamento (do fim para o início):
-
-        # i=4 <- valor da posição 3 (E)
-        # i=3 <- valor da posição 2 (C)
-        # i=2 <- valor da posição 1 (B)
-        # i=1 <- valor da posição 0 (A)
-
-        # Depois de inserir 'F' no início:
-        # Depois (quant=5):
-
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  F  ||  A  ||  B  ||  C  ||  E  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
             
     def remover_inicio(self):
         
@@ -169,41 +43,25 @@ class Les:
         else:
             for i in range(self.quant - 1):
                 self.vetor[i] = self.vetor[i + 1]
-            # for i in range(1, self.quant):
-            #     self.vetor[i - 1] = self.vetor[i]
-            # self.vetor[self.quant - 1] = None # Limpa a última posição
             self.quant -= 1
         
-        # Remove o primeiro elemento da lista, movendo todos para a esquerda.
-
-        # Exemplo:
-
-        # Antes (quant=5):
-
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  F  ||  A  ||  B  ||  C  ||  E  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-
-        # Passo a passo do deslocamento:
-
-        # i=1 -> posição 0 recebe 'A'
-        # i=2 -> posição 1 recebe 'B'
-        # i=3 -> posição 2 recebe 'C'
-        # i=4 -> posição 3 recebe 'E'
-
-        # Depois da remoção:
-        # depois (quant=4):
-
-        #  Índices:
-        #   _____  _____  _____  _____  _____
-        #  |  0  ||  1  ||  2  ||  3  ||  4  |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-        #  Valores:
-        #   _____  _____  _____  _____  _____
-        #  |  A  ||  B  ||  C  ||  E  ||     |
-        #   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    def remover(self, valor):
+        pos = 999
+        if self.quant == 0:
+            print('A lista está vazia')
+        else:
+            if valor in self.vetor:
+                for i in range(self.quant):
+                    if valor == self.vetor[i]:
+                        pos = i
+                        for i in range(pos, self.quant - 1):
+                            self.vetor[i] = self.vetor[i + 1]
+                            self.quant -= 1
+                            # break - coloquei para que caso haja repetição
+            else:
+                print('Valor não existe ou não foi encontrado')
+                
+    def removerTF(self, valor):
+        pass
+    def remover_contar(self, valor):
+        pass
