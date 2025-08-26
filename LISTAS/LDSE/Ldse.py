bcc20bc6446c4ef16653c022dcbb2fcc72a676ed
NettoZk <adeuvaldon7@rede.ulbra.br>
2025-08-25 21:25:24 -0300

    Ldse, inicio

---
+++ LISTAS/LDSE/Ldse.py
@@ -0,0 +1,18 @@
+class No:
+    
+    def __init__(self, valor, proximo):
+        self.info = valor
+        self.prox = proximo
+        
+class Ldse:
+    
+    def __init__(self):
+        self.prim = self.ult = None
+        self.quant = 0
+        
+    def inserir_inicio(self, valor):
+        if self.quant == 0:
+            self.prim = self.ult = No(valor, None)
+        else:
+            self.prim = No(valor, self.prim)
+        self.quant += 1
---
+++ LISTAS/LDSE/usaLdse.py
@@ -0,0 +1,6 @@
+import Ldse
+
+l = Ldse.Ldse()
+
+l.inserir_inicio('A')
+l.inserir_inicio('B')
