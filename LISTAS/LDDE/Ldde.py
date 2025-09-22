8338bc7c5296b3de2ba722b285e09cd143441902
NettoZk <adeuvaldon7@gmail.com>
2025-09-22 11:15:10 -0300

    uppp

--- LISTAS/LDDE/Ldde.py
+++ LISTAS/LDDE/Ldde.py
@@ -93,36 +93,35 @@ class Ldde:
             
         self.quant -= 1        
 
-
-    # Inserindo o removerTF: retorna True se removeu um valor, e false se não removeu.
+    
     def removerTF(self, valor):
         aux = self.prim
+        
         if self.quant == 1 and aux.info == valor:
             self.prim = self.ult = None
             self.quant -= 1
-
             return True
-       
+        
         while aux != None and aux.info != valor:
             aux = aux.prox
+            
         if aux == None:
-            print("Valor não está na lista.")
-            return print(False)
-        if aux.ant == None:
-            aux = self.prim = aux.prox
-            self.prim.ant = None    
-        elif aux.prox == None:
-            aux = self.ult = aux.ant
+            return False
+        if aux == self.prim and aux == self.ult:
+            self.prim = self.ult = None
+        elif aux == self.prim:
+            self.prim = self.prim.prox
+            self.prim.ant = None
+        elif aux == self.ult:
+            self.ult = self.ult.ant
             self.ult.prox = None
         else:
             aux.ant.prox = aux.prox
             aux.prox.ant = aux.ant
+            
+        self.quant -= 1 
+        return True
     
-        self.quant -= 1
-        print(f"Valor '{valor}' removido. ")                
-        return print(True)
-    
-    # Inserindo o metodo remover_contar:
     def remover_contar(self, valor):
         cont = 0
         aux = self.prim
@@ -145,4 +144,4 @@ class Ldde:
                     cont += 1
                     self.quant -= 1
                 aux = aux.prox
-            print(f"O valor {valor} foi removido {cont} vezes")
+        print(f"O valor {valor} foi removido {cont} vez(es)")
