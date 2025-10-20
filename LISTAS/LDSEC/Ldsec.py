97c2026539583d3fb8c7c3f874dad3c2bcf4e1d1
NettoZk <adeuvaldon7@rede.ulbra.br>
2025-10-20 19:32:03 -0300

    Ldsec

---
+++ LISTAS/LDSEC/Ldsec.py
@@ -0,0 +1,19 @@
+class No:
+    
+    def __init__(self, valor, proximo):
+        self.info = valor
+        self.prox = proximo
+        
+class Ldsec:
+    
+    def __init__(self):
+        self.prim = self.ult = None
+        self.quant = 0
+        
+    def inserir_inicio(self, valor):
+        if self.quant == 0:
+            self.prim = self.ult = No(valor, None)
+            self.ult.prox = self.prim
+        else:
+            self.prim = self.ult.prox = No(valor, self.prim)
+        self.quant += 1
